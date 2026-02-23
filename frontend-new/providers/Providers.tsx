ecc47b33d9d152200cb0dac5dbd2f15147fd69ba
Priyanshu sahu <sahupriyanshu108@gmail.com>
2026-02-23 22:18:00 +0530

    role fix

--- frontend-new/providers/Providers.tsx
+++ frontend-new/providers/Providers.tsx
@@ -14,7 +14,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       theme={{
         primaryColor: "indigo",
       }}
-      defaultColorScheme="dark"
+    
     >
       <QueryClientProvider client={queryClient}>
         <Notifications position="top-right" />
